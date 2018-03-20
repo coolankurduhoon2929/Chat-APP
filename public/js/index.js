c9c1ced255a892a41cf517b8ad42bbedbff3ed0e
Ankur Duhoon <coolankurduhoon2929@gmail.com>
2018-03-21 01:41:38 +0530

    geolocation added

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,15 @@ socket.on('newMessage',function(message){
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage',function(message){
+  var li=$('<li></li>');
+  var a=$('<a target="_blank">My current Location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 //Event listener
 $('#message-form').on('submit',function(e){
   //prevent page from reloading...
@@ -28,4 +37,20 @@ $('#message-form').on('submit',function(e){
     });
     $('[name=message]').val("");
   }
+});
+
+
+var locationButton=$('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  },function(){
+    alert('Unable to fetch location');
+  });
 });
