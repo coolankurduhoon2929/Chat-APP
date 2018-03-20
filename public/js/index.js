cacd1c853238c8901da679cd419a937ea9733020
Ankur Duhoon <coolankurduhoon2929@gmail.com>
2018-03-20 23:41:51 +0530

    first ui changes

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,23 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
   console.log('Message',message);
+  var li=$('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
+
+//Event listener
+$('#message-form').on('submit',function(e){
+  //prevent page from reloading...
+  e.preventDefault();
+  //Sending message to server...
+  if($('[name=message]').val()!==""){
+    socket.emit('createMessage',{
+      from:"Akash",
+      text:$('[name=message]').val()
+    },function(data){
+      console.log('Got it',data);
+    });
+    $('[name=message]').val("");
+  }
 });
