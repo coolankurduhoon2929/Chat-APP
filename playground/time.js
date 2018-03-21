103dbfb71211f7fd07c49e74d3ba12763094bfa9
Ankur Duhoon <coolankurduhoon2929@gmail.com>
2018-03-21 21:29:14 +0530

    time added

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment=require('moment');
+
+
+// console.log(new Date().getMonth()+1);
+// var time=new Date().getTime();
+
+// var date=moment();
+// date.add(1,'y');
+// console.log(date.format('Do MMM YYYY'));
+
+var date=moment(new Date().getTime());
+console.log(date);
