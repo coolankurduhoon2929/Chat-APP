3131618c4a4b884090622f832e4ad1a10442ce04
Ankur Duhoon <coolankurduhoon2929@gmail.com>
2018-03-22 20:07:56 +0530

    autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket=io();
+
+function scrollToBottom(){
+  //Selectors
+  var messages=$('#messages');
+  var newMessage=messages.children('li:last-child');
+  //Heights
+  var clientHeight=messages.prop('clientHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect',function(){
   console.log("Connected");
 });
@@ -16,6 +34,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 
   // // console.log('Message',message);
   // var li=$('<li></li>');
@@ -32,7 +51,8 @@ socket.on('newLocationMessage',function(message){
     createdAt:formattedTime
   });
   $('#messages').append(html);
-  
+  scrollToBottom();
+
   // var li=$('<li></li>');
   // var a=$('<a target="_blank">My current Location</a>');
   // li.text(`${message.from} ${formattedTime}: `);
